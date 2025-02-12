79c1018fb9e25b5bc0cab2e651cc6c776258e933
RafaelFantato <rafael.fantato@lcs.valantic.com>
2025-02-12 15:14:16 +0000

    fixes

---
+++ src/set-new-version.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+const path = require("path");
+const xml2js = require("xml2js");
+const semver = require("semver");
+
+const builder = new xml2js.Builder({
+    xmldec: {
+        version: "1.0",
+        encoding: "UTF-8"
+    }
+});
+
+async function replaceIcons(context) {
+    const projectRoot = context.opts.projectRoot;
+
+    const usesNewStructure = fs.existsSync(path.join(projectRoot, "platforms", "android", "app"));
+    const basePath = usesNewStructure
+        ? path.join(projectRoot, "platforms", "android", "app", "src", "main")
+        : path.join(projectRoot, "platforms", "android");
+
+    const configXmlPath = path.join(projectRoot, "config.xml");
+
+    console.log(`[New Version] projectRoot: ${projectRoot}`);
+
+    // Verifica se o config.xml existe
+    if (!fs.existsSync(configXmlPath)) {
+        console.error(`[New Version] Arquivo config.xml não encontrado em: ${configXmlPath}`);
+        return;
+    }
+
+    // Obtém a versão do NEW_VERSION (preferência do Cordova)
+    const newVersion = getConfigParser(context, configXmlPath).getPreference("NEW_VERSION") || "";
+
+    if (!newVersion) {
+        console.log("[New Version] Variável NEW_VERSION não definida ou inválida. Nenhuma ação será realizada.");
+        return;
+    }
+
+    try {
+        const xmlContent = fs.readFileSync(configXmlPath, "utf-8");
+        const parser = new xml2js.Parser();
+
+        const result = await parser.parseStringPromise(xmlContent);
+
+        // Remove a preferência 'NEW_VERSION' caso exista
+        if (result.widget.preference) {
+            const indexNEW_VERSION = result.widget.preference.findIndex(p => p.$.name === "NEW_VERSION");
+            if (indexNEW_VERSION !== -1) {
+                result.widget.preference.splice(indexNEW_VERSION, 1);
+                console.log('[New Version] Preferência "NEW_VERSION" removida com sucesso.');
+            } else {
+                console.log('[New Version] Preferência "NEW_VERSION" não encontrada.');
+            }
+        }
+
+        // Atualiza a versão do app
+        result.widget.$.version = newVersion;
+
+        // Atualiza o versionCode para Android (se existir)
+        if (result.widget.$["android-versionCode"]) {
+            const newVersionCode = parseInt(result.widget.$["android-versionCode"]) + 1;
+            result.widget.$["android-versionCode"] = newVersionCode.toString();
+            console.log(`[New Version] android-versionCode atualizado para ${newVersionCode}`);
+        }
+
+        // Salva as alterações no config.xml
+        const newXmlContent = builder.buildObject(result);
+        fs.writeFileSync(configXmlPath, newXmlContent, "utf-8");
+
+        console.log("[New Version] Arquivo config.xml modificado com sucesso!");
+    } catch (err) {
+        console.error("[New Version] Erro ao modificar o config.xml:", err);
+    }
+}
+
+function getConfigParser(context, configPath) {
+    let ConfigParser;
+
+    if (semver.lt(context.opts.cordova.version, "5.4.0")) {
+        ConfigParser = context.requireCordovaModule("cordova-lib/src/ConfigParser/ConfigParser");
+    } else {
+        ConfigParser = require("cordova-common").ConfigParser;
+    }
+
+    return new ConfigParser(configPath);
+}
+
+module.exports = replaceIcons;
